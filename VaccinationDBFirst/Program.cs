034de2a1cb96d7c173db2dc107b74a1b58a0d6ac
RebeccaLapointe <RebeccaLapointe@INFO-386-2-P01>
2025-01-09 20:10:38 -0500

    Ajoutez des fichiers projet.

---
+++ VaccinationDBFirst/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace VaccinationDBFirst
+{
+    internal class Program
+    {
+        static void Main(string[] _)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
